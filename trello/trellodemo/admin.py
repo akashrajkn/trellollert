37ea785b043f0e6b621a85ee5364aac6ebbce5d2
Bohemian Rhapsody <adityamaharana7@gmail.com>
2016-07-18 17:07:23 +0530

    Schema setup

--- trello/trellodemo/admin.py
+++ trello/trellodemo/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Board, Card
+
+class CardsInline(admin.TabularInline):
+	model = Card
+	extra = 1
+
+class BoardAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields' : ['board_text']}),
+		('Created By', {'fields' : ['created_by']}),
+		('Created On', {'fields' : ['created_time']}),
+		('Last Modified', {'fields' : ['last_modified']}),		
+	]
+	inlines = [CardsInline]
+
+	list_display = ('board_text', 'created_by', 'created_time')
+	list_filter = ['created_time']
+	search_fields = ['created_by']
+admin.site.register(Board, BoardAdmin)
--- trello/trellodemo/models.py
+++ trello/trellodemo/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+	board_text = models.CharField(max_length=255)
+	created_by = models.CharField(max_length=255)
+	created_time = models.DateTimeField('Time of Creation')
+	last_modified = models.DateTimeField('Last Modified')
+
+	def __str__(self):
+		return self.board_text
+
+class Card(models.Model):
+	board = models.ForeignKey(Board, on_delete=models.CASCADE)
+	card_text = models.CharField(max_length=255)
+	created_by = models.CharField(max_length=255)
+	created_time = models.DateTimeField('Time of Creation')
+	last_modified = models.DateTimeField('Last Modified')
+
+	def __str__(self):
+		return self.card_text
+
+class List(models.Model):
+	card = models.ForeignKey(Card, on_delete=models.CASCADE)
+	title = models.CharField(max_length=255)
+	description = models.CharField(max_length=1000)
+	created_by = models.CharField(max_length=255)
+	created_time = models.DateTimeField('Time of Creation')
+	last_modified = models.DateTimeField('Last Modified')
+
+	def __str__(self):
+		return self.title
