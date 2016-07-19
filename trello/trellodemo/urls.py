d339d6ad02f5c6e50f9aee05159419f8f71a6bbc
akash-linux-gor <akashrajkn@gmail.com>
2016-07-19 10:52:01 +0530

    added basic board page

--- trello/trellodemo/urls.py
+++ trello/trellodemo/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'trellodemo'
 urlpatterns = [
-	url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	url(r'^(?P<board_id>[0-9]+)/$', views.board, name='board'),
 ]
--- trello/trellodemo/views.py
+++ trello/trellodemo/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from .models import Board
+
 @login_required
 def index(request):
-	return HttpResponse("Who let the dogs out?")
+	board_list = Board.objects.all()
+	context = {'board_list': board_list}
+	return render(request, 'trellodemo/index.html', context)
+
+@login_required
+def board(request, board_id):
+	board = get_object_or_404(Board, pk=board_id)
+	return render(request, 'trellodemo/board.html', {'board':board})
