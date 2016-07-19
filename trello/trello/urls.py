66c202b676e921b3e3b82223986afaa436995b0d
rishi2893 <rishi2893@gmail.com>
2016-07-19 11:17:00 +0530

    Initialized trello proj

--- trello/trello/urls.py
+++ trello/trello/urls.py
@@ -1,7 +1,7 @@
 """trello URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,14 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-from . import views as home_views
 
 urlpatterns = [
-	url(r'^$', home_views.index, name='index'),
-	url(r'^trello/', include('trellodemo.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login', auth_views.login)
 ]
