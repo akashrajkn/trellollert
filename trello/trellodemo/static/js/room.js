0592e60aaca2b7fb3ec540ed90bff33fdaeb4508
akash-linux-gor <akashrajkn@gmail.com>
2016-07-26 10:10:23 +0530

    empty

--- trello/trellodemo/static/js/room.js
+++ trello/trellodemo/static/js/room.js
@@ -15,6 +15,51 @@ var OverlayTrigger = ReactBootstrap.OverlayTrigger;
 var Overlay = ReactBootstrap.Overlay;
 
 
+var SearchExample = React.createClass({
+
+    getInitialState: function(){
+        return { searchString: '' };
+    },
+
+    handleChange: function(e){
+
+        this.setState({searchString:e.target.value});
+    },
+
+    render: function() {
+
+        var libraries = this.props.items,
+            searchString = this.state.searchString.trim().toLowerCase();
+
+
+        if(searchString.length > 0){
+
+            // We are searching. Filter the results.
+
+            libraries = libraries.filter(function(l){
+                return l.name.toLowerCase().match( searchString );
+            });
+
+        }
+
+        return <div>
+                    <input type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Type here" />
+
+                    <ul> 
+
+                        { libraries.map(function(l){
+                            return <li>{l.name} <a href={l.url}>{l.url}</a></li>
+                        }) }
+
+                    </ul>
+
+                </div>;
+
+    }
+});
+
+
+
 var Room = React.createClass({
   loadBoardsFromServer: function() {
 //    var user = document.getElementById('alpha').innerHTML;
